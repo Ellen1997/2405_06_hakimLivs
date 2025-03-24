e1477d606c54d6f5b6ebaa65fb2d3f911e832fda
yolandaatt <yolandaatteyeh3@gmail.com>
2025-03-24 13:08:03 +0100

    API/products

---
+++ src/routes/mongproducts.js
@@ -0,0 +1,130 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const mongoproducts = require("../models/mongoproducts.js");
+
+
+const router = express.Router();
+
+
+
+
+//GET metod
+router.get('/', async (req, res) => {
+    try {
+        const products = await mongoproducts.find();
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).send({ message: "Något gick fel", error: error.message });
+    }
+});
+
+
+//GET by ID
+router.get('/:id', async (req, res) => {
+    try {
+
+
+        const product = await mongoproducts.findById(req.params.id);
+
+
+        if (!product) {
+            return res.send(404).send({message: 'Produkt inte hittad!'})
+        }
+
+
+        res.status(200).json(product);
+
+
+    }catch (error) {
+        res.status(500).send({message: 'Något gick fel', error})
+    }
+})
+
+
+//POST
+router.post('/', async (req, res) => {
+    try {
+        const {name, price, description, stock, category} = req.body;
+
+
+        const newProduct = new mongoproducts({
+            name,
+            price,
+            description,
+            stock,
+            category
+        });
+
+
+        await newProduct.save();
+        res.status(201).send({ message: "Produkt skapad", product: newProduct });
+
+
+    } catch (error) {
+        res.status(500).send({ message: "Något gick fel, produkt ej inlagd", error: error.message});
+    }
+});
+
+
+//PUT
+router.put('/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const { name, description, price, stock, category } = req.body;
+
+
+        const product = await mongoproducts.findById(id)
+
+
+        if (!product) {
+            return res.status(404).json({message: 'Produkt kunde ej hittas!'})
+        }
+
+
+        product.name = name || product.name;
+        product.description = description || product.description;
+        product.price = price || product.price;
+        product.stock = stock || product.stock;
+        product.category = category || product.category;
+
+
+        await product.save();
+
+
+        res.status(200).json({message: 'Produkten uppdaterad!', product})
+
+
+    }catch (error) {
+        res.status(500).send({message: 'Något gick fel', error})
+    }
+})
+
+
+//DELETE
+router.delete('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+
+
+        const result = await mongoproducts.deleteOne({_id: id})
+
+
+
+
+        if (result.deletedCount === 0) {
+            return res.status(404).json({error: 'Produkten hittas inte!'})
+        }
+
+
+        res.status(200).json({ message: 'Produkten borttagen' });
+
+
+
+
+    }catch (error) {
+        res.status(500).send({ message: "Något gick fel", error: error.message });
+    }
+})
+
+
+module.exports = router;
--- src/server.js
+++ src/server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const dbTestRoute = require("./routes/dbTest.js");
+const mongproductsRoute = require("./routes/mongproducts.js");
 const path = require("path");
 const mongoproducts = require("./models/mongoproducts.js");
+
 
 dotenv.config();
 
@@ -13,81 +15,23 @@ const PORT = process.env.PORT || 5500;
 
 app.use(express.json());
 
+
 mongoose.connect("mongodb+srv://ellenholmgren:Y71UhxXQpLUTf15J@expresscrash.vbreq.mongodb.net/?retryWrites=true&w=majority&appName=expresscrash")
-.then(() => {
-    console.log("connected to MongoDB ATLAS");
-    app.listen(PORT, () => {
-        console.log(`Servern är uppe och körs på http://localhost:${PORT}`)
-    });
-})
-.catch ((e) => 
-    console.error("MongoDB atlas connection error:", e)
-);
-
-// run()
-async function run() {
-
-    try {
-        const mongoproduct1 = await mongoproducts.create({
-
-            _id: 4,
-            name: "Ananas",
-            price: 19.99,
-            description: "En god exotisk frukt",
-            stock: 100,
-            category: "Fruit"
-
-        })
-        console.log(mongoproduct1)
-    } catch (error) {
-        console.log(error.message)
-    }
-}
-
-app.post('/products', async (req, res) => {
-    try {
-        const {name, price, description, stock} = req.body;
-
-        const newProduct = new mongoproducts({
-            name, 
-            price, 
-            description,
-            stock,
-            category
+    .then(() => {
+        console.log("connected to MongoDB ATLAS");
+        app.listen(PORT, () => {
+            console.log(`Servern är uppe och körs på http://localhost:${PORT}`);
         });
+    })
+    .catch((e) =>
+        console.error("MongoDB atlas connection error:", e)
+    );
 
-        await newProduct.save();
-        res.status(201).send({ message: "Produkt skapad", product: newProduct });
-
-
-    } catch (error) {
-        res.status(500).send({ message: "Något gick fel, produkt ej inlagd", error: error.message});
-    }
-})
-
-app.get('/products', async (req, res) => {
-    try {
-       
-        const products = await mongoproducts.find();
-        res.status(200).json(products);
-    } catch (error) {
-        res.status(500).send({ message: "Något gick fel", error: error.message });
-    }
-});
-
-// app.use("/test-db", dbTestRoute);
+app.use("/products", mongproductsRoute);
+app.use("/test-db", dbTestRoute);
 
 app.use(express.static(path.resolve(__dirname, '..')));
-
-
-app.get("/hej", (req, res) => {
-    res.send("express servern/MongoDB är uppe")
-});
 
 app.get('/hem', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'index.html'));
 });
-
-
-
-
