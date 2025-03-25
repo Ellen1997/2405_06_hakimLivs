fe633aa9c182f791352d0ae7db37e849c9462ead
Ellen1997 <ellen-holmgren@yh.nackademin.se>
2025-03-25 15:36:47 +0100

    testar lite ändringar pga vercel-länken måste funka med /products och inte bara localhost så frontend kan få tillgång

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const mongproductsRoute = require("./routes/mongproducts.js");
@@ -10,17 +11,18 @@ dotenv.config();
 
 const app = express();
 
-const PORT = process.env.PORT || 5500;
+// const PORT = process.env.PORT || 5500;
 
+app.use(cors());
 app.use(express.json());
 
 
 mongoose.connect("mongodb+srv://ellenholmgren:Y71UhxXQpLUTf15J@expresscrash.vbreq.mongodb.net/?retryWrites=true&w=majority&appName=expresscrash")
     .then(() => {
         console.log("connected to MongoDB ATLAS");
-        app.listen(PORT, () => {
-            console.log(`Servern är uppe och körs på http://localhost:${PORT}`);
-        });
+        // app.listen(PORT, () => {
+        //     console.log(`Servern är uppe och körs på http://localhost:${PORT}`);
+        // });
     })
     .catch((e) =>
         console.error("MongoDB atlas connection error:", e)
@@ -28,14 +30,18 @@ mongoose.connect("mongodb+srv://ellenholmgren:Y71UhxXQpLUTf15J@expresscrash.vbre
 
 app.use("/products", mongproductsRoute);
 
-app.use(express.static(path.resolve(__dirname, '..')));
+// app.use(express.static(path.resolve(__dirname, '..')));
 
 //denna app.get '/hem' under behövs inte egentligen, den är bara där från en test grej. 
 // Tydligen om inget annat är definerat så söker servern efter ett index.html
-app.get('/hem', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'index.html'));
-});
+
+
+// app.get('/hem', (req, res) => {
+//     res.sendFile(path.join(__dirname, '..', 'index.html'));
+// });
 
 app.get('/test', (req, res) => {
     res.sendFile(path.join(__dirname, '__tests__', 'indexxx.html'));
 }); 
+
+module.exports = app;
